dcd59055de464d1c926741d4871d424ed1ad6575
Oscar Skean <ofskean727@gmail.com>
2024-10-24 14:15:01 -0400

    Updated raceday text

--- components/data_display/Racedays/RacedayContainerList.vue
+++ components/data_display/Racedays/RacedayContainerList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-4">
-    <h1 class="text-2xl font-bold mb-4">Race Selection</h1>
+    <h1 class="text-2xl font-bold mb-4">Select your track</h1>
 
 
     <div v-if="loading" class="mt-4">Loading races...</div>
